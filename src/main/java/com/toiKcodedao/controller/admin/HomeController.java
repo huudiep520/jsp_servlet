4dad48c06501f820c9e057a43cc2d125915e69e7
huudiep520 <huudiep520@gmail.com>
2021-07-28 20:54:51 +0900

    新規作成

---
+++ src/main/java/com/toiKcodedao/controller/admin/HomeController.java
@@ -0,0 +1,24 @@
+package com.toiKcodedao.controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/admin-home"})
+public class HomeController extends HttpServlet {
+
+	private static final long serialVersionUID = 5778363859080588649L;
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+}
